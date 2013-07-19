ebf8784438896c76576a459694d3d0ac5e7f8088
Josh Shaloo <shaloo@email.arizona.edu>
2013-07-19 16:08:51 -0700

    committing

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,16 @@
 set :application, 'capistrano testing'
-set :repo_url, 'https://jshalooua@bitbucket.org/jshalooua/capistrano-testing.git'
+set :repo_url, 'https://github.com/joshuofa/capistrano-testing.git'
 #set :ssh_options, {:forward_agent => true}
 
 #we will set this in the evnironment specific deployment files
 #ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
-set :deploy_to, "/opt/kuali/rhubarb"
+set :deploy_to, "/opt/kuali/rhubarb/rhubarb-1.0"
 set :scm, :git
 
 set :format, :pretty
 set :log_level, :debug
-# set :pty, true
+set :pty, true
 
 # set :linked_files, %w{config/database.yml}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
